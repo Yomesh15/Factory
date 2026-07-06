e695d5b42c702a247cc86a941f0b0e91914630d2
Yomesh Nagar <yomeshnagar2006@gmail.com>
2026-07-06 12:00:11 +0530

    Factory

--- Backend/config/mail.js
+++ Backend/config/mail.js
@@ -1,11 +1,17 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 export default transporter;
